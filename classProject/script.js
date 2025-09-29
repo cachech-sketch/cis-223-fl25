bccac71a2e6a26a87bed7c603c7e9c82f9280373
cachech-sketch <cachech@gmail.com>
2025-09-28 23:12:59 -0500

    it a fuction that check to see how many times it be watered

--- classProject/script.js
+++ classProject/script.js
@@ -4,6 +4,15 @@
 // Becky
 //Initialize a variable to store the number of times the current flower has been watered using an initial value of 0.
 let timesWatered = 0;
+//Write a function that checks how many times the current flower has been watered (timesWatered variable). If the number is equal to the waterCap, place a flower image at the top of the stem.
+function myCheck(){  timesWatered=0
+    if (timesWatered =waterCap)
+        return console.log ("you have  grown  a flower!")
+    // i dont know how  to add a flower
+
+    
+}
+
 // Ben
 
 // Eduardo
