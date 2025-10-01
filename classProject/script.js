da360e7c0bf55d7ac898751140fce1c4c3e5440b
cachech-sketch <cachech@gmail.com>
2025-10-01 10:11:42 -0500

    update 1.3f

--- classProject/script.js
+++ classProject/script.js
@@ -2,15 +2,12 @@
 // MEMORY INITIALIZATION
 
 // Becky
-//Initialize a variable to store the number of times the current flower has been watered using an initial value of 0.
 let timesWatered = 0;
-//Write a function that checks how many times the current flower has been watered (timesWatered variable). If the number is equal to the waterCap, place a flower image at the top of the stem.
-function myCheck(){  timesWatered=0
+let waterCap= 3
+function myCheck(){  
     if (timesWatered ===waterCap)
-       return anchor.y.flower=20
-        // return console.log ("you have  grown  a flower!")
-    // i dont know how  to add a flower
-
+        anchor.y.flower=20
+       
     
 }
 
