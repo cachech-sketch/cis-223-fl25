1fa9806965029e07c29ba7aeb1ccbca5eaf8e3e1
cachech-sketch <cachech@gmail.com>
2025-10-01 10:13:04 -0500

    updat23

--- classProject/script.js
+++ classProject/script.js
@@ -6,10 +6,8 @@ let timesWatered = 0;
 let waterCap= 3
 function myCheck(){  
     if (timesWatered ===waterCap)
-       return anchor.y.flower=20
-        // return console.log ("you have  grown  a flower!")
-    // i dont know how  to add a flower
-
+       anchor.y.flower=20
+     
     
 }
 
